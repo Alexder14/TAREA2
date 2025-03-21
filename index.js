8f05cb4d18c23c68d78f58aaf5e1c7bd0fb1e1ac
Alexander Salazar <1817571250101@ingenieria.usac.edu.gt>
2025-03-20 23:32:11 -0600

    fix

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos desde la carpeta 'public'
+// Servir los archivos estáticos de la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint que retorna la información
 app.get('/info', (req, res) => {
   const data = {
-    nombre: 'Tu Nombre',
-    carnet: '12345678',
-    curso: 'Nombre del Curso'
+    nombre: 'Alexander Salazar',
+    carnet: '201020600',
+    curso: 'Curso de Node.js' // Cambia el nombre del curso si necesitas
   };
   res.json(data);
 });
